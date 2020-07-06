7383b6b51acc2844f3d85b5bac208715ebfae581
jungu009 <f644716003@live.com>
2020-07-06 09:33:02 +0800

    add feature1 on feature-1 branch

--- app/src/main/java/cn/fc/gitstudy/MainActivity.java
+++ app/src/main/java/cn/fc/gitstudy/MainActivity.java
@@ -3,6 +3,8 @@ package cn.fc.gitstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                feature1(v);
+            }
+        });
+    }
+
+    public void feature1(View view){
+        Toast.makeText(this, "功能1", Toast.LENGTH_SHORT).show();
     }
 }
